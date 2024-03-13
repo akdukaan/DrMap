c6453f626eca939d0d752b7326abdfbdba9bab41
Kaan <drbot7@gmail.com>
2024-03-13 00:04:52 -0500

    mostly comments with a little more fixes

--- src/main/java/org/acornmc/drmap/listener/BukkitListener.java
+++ src/main/java/org/acornmc/drmap/listener/BukkitListener.java
@@ -22,6 +22,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 
 
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class BukkitListener implements Listener {
@@ -64,21 +65,37 @@ public class BukkitListener implements Listener {
                 return;
             }
             if (player.hasPermission("drmap.place.magic")) {
-                tryMagicPlace(event, frame, hand);
+                if (tryMagicPlace(player, frame, hand)) {
+                    // tryMagicPlace will already place the map if it succeeds.
+                    // If we don't cancel the event, it'll rotate the one map.
+                    event.setCancelled(true);
+                }
             }
         }
     }
 
-    public void tryMagicPlace(PlayerInteractEntityEvent event, ItemFrame frame, ItemStack itemUsed) {
-        Player player = event.getPlayer();
+    /**
+     *
+     * @param frame the item frame that the player manually placed. We will try to magic place around it.
+     * @param itemUsed the map that the player placed in the item frame. We will use its meta to find other maps like it.
+     * @return true if we were able to magic-place everything
+     */
+    public boolean tryMagicPlace(Player player, ItemFrame frame, ItemStack itemUsed) {
         ItemMeta itemMeta = itemUsed.getItemMeta();
-        if (itemMeta == null) return;
+        if (itemMeta == null) return false;
         PersistentDataContainer container = itemMeta.getPersistentDataContainer();
         int[] part = PictureMeta.getParts(container);
-        if (part[2] == 0 && part[3] == 0) return;
+        if (part[2] == 0 && part[3] == 0) return false;
 
         // Check that the player has all parts
+        // partsFound keeps track of which spot in the player's inventory we can find each part of the map.
+        // If we don't know or it doesnt exist, it's set to -1.
         int[][] partsFound = new int[part[2]+1][part[3]+1];
+        // Initialize the array with all -1s to indicate that each part is not found.
+        for (int i = 0; i < partsFound.length; i++) {
+            Arrays.fill(partsFound[i], -1);
+        }
+        // Loop through the player's inventory and indicate which inventory spot we found the map in.
         String source = PictureMeta.getSource(container);
         long creation = PictureMeta.getCreationLong(container);
         String author = PictureMeta.getAuthorUUIDString(container);
@@ -87,26 +104,26 @@ public class BukkitListener implements Listener {
             ItemStack item = playerInventory.getItem(inventoryIndex);
             partsFound = markIfFound(item, partsFound, inventoryIndex, source, creation, author);
         }
-        if (!allFound(partsFound)) return;
+        if (!allFound(partsFound)) return false;
 
         // Check that the wall has all the empty itemframes
-        int leftSearch = part[0];
-        int upSearch = part[1];
-        int rightSearch = part[2]-part[0];
-        int downSearch = part[3]-part[1];
+        int leftSearch = part[0]; // The distance to the 'left' we need to check item frames
+        int upSearch = part[1]; // The distance 'up' we need to check item frames
+        int rightSearch = part[2]-part[0]; // The distance to the 'right' we need to check item frames
+        int downSearch = part[3]-part[1]; // The distance down we need to check item frames
         Location origin = frame.getLocation();
         World world = origin.getWorld();
-        if (world == null) return;
+        if (world == null) return false;
         int originX = origin.getBlockX();
         int originZ = origin.getBlockZ();
         BlockFace face = frame.getFacing();
+        int lowestZ,lowestX,highestZ,highestX;
+        int lowestY = origin.getBlockY()-downSearch;
+        int highestY = origin.getBlockY()+upSearch;
         // North: Left is increasing x, Right is decreasing x,
         // South: Left is decreasing x, Right is increasing x,
         // West: Left is decreasing z, Right is increasing z,
         // East Left is increasing z, Right is decreasing z,
-        int lowestZ,lowestX,highestZ,highestX;
-        int lowestY = origin.getBlockY()-downSearch;
-        int highestY = origin.getBlockY()+upSearch;
         if (face == BlockFace.NORTH) {
             lowestZ = originZ;
             highestZ = originZ;
@@ -122,22 +139,28 @@ public class BukkitListener implements Listener {
             highestX = originX;
             lowestZ = originZ - leftSearch;
             highestZ = originZ + rightSearch;
-        } else { // BlockFace.EAST
+        } else if (face == BlockFace.EAST){
             lowestX = originX;
             highestX = originX;
             lowestZ = originZ - rightSearch;
             highestZ = originZ + leftSearch;
+        } else {
+            // We will not support blockfaces UP or DOWN at the moment
+            // and I don't think the other ones are possible.
+            return false;
         }
         for (int x = lowestX; x <= highestX; x++) {
             for (int y = lowestY; y <= highestY; y++) {
                 for (int z = lowestZ; z <= highestZ; z++) {
                     Location location = new Location(world, x, y, z);
-                    if (Util.getBlockProtection(player, location, Material.ITEM_FRAME) != null) return;
-                    if (getEmptyItemFrame(location, world, face) == null) return;
+                    if (Util.getBlockProtection(player, location, Material.ITEM_FRAME) != null) return false;
+                    if (getEmptyItemFrame(location, world, face) == null) return false;
                 }
             }
         }
 
+        // At this point we've checked everything we need to know that we should magic place.
+        // So we will go through each location and place the item frame we need
         if (face == BlockFace.NORTH) {
             for (int x = lowestX; x <= highestX; x++) {
                 for (int y = lowestY; y <= highestY; y++) {
@@ -154,7 +177,7 @@ public class BukkitListener implements Listener {
                 for (int y = lowestY; y <= highestY; y++) {
                     Location location = new Location(world, x, y, originZ);
                     ItemFrame itemFrame = getEmptyItemFrame(location, world, face);
-                    int inventoryIndex = partsFound[x][y];
+                    int inventoryIndex = partsFound[x-lowestX][y-lowestY];
                     ItemStack map = playerInventory.getItem(inventoryIndex);
                     itemFrame.setItem(map);
                     removeOne(playerInventory,inventoryIndex);
@@ -165,8 +188,8 @@ public class BukkitListener implements Listener {
                 for (int y = lowestY; y <= highestY; y++) {
                     Location location = new Location(world, originX, y, z);
                     ItemFrame itemFrame = getEmptyItemFrame(location, world, face);
-                    int inventoryIndex = partsFound[z][y];
-                    ItemStack map = playerInventory.getItem(partsFound[z][y]);
+                    int inventoryIndex = partsFound[z-lowestZ][y-lowestY];
+                    ItemStack map = playerInventory.getItem(inventoryIndex);
                     itemFrame.setItem(map);
                     removeOne(playerInventory,inventoryIndex);
                 }
@@ -183,6 +206,7 @@ public class BukkitListener implements Listener {
                 }
             }
         }
+        return true;
     }
 
     public void removeOne(PlayerInventory inv, int index) {
@@ -191,6 +215,13 @@ public class BukkitListener implements Listener {
         itemStack.setAmount(itemStack.getAmount()-1);
     }
 
+    /**
+     *
+     * @param location The location to check
+     * @param world The world to check
+     * @param face The blockface we want to find
+     * @return the itemFrame at the location with the blockface or null if none exists.
+     */
     public ItemFrame getEmptyItemFrame(Location location, World world, BlockFace face) {
         Predicate<Entity> isItemFrame = entity -> entity.getFacing() == face && (entity.getType() == EntityType.ITEM_FRAME || entity.getType() == EntityType.GLOW_ITEM_FRAME);
         for (Entity frame : world.getNearbyEntities(location,1,1,1,isItemFrame)) {
@@ -200,6 +231,11 @@ public class BukkitListener implements Listener {
         return null;
     }
 
+    /**
+     *
+     * @param array a 2d array to check
+     * @return true if all values in the array are non-negative
+     */
     public static boolean allFound(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
@@ -211,6 +247,16 @@ public class BukkitListener implements Listener {
         return true;
     }
 
+    /**
+     *
+     * @param item The item to potentially add to partsFound
+     * @param partsFound What we know about the found parts so far
+     * @param inventoryIndex The location indicator to save into partsFound if the item param is a match
+     * @param source The map's source url to match to
+     * @param creation The map's creation time to match to
+     * @param author The map's author to match to
+     * @return the updated version of partsFound after adding in info from the item param
+     */
     public int[][] markIfFound(ItemStack item, int[][] partsFound, int inventoryIndex, String source, long creation, String author) {
         if (!Util.isDrMap(item)) return partsFound;
         ItemMeta itemMeta = item.getItemMeta();
@@ -222,6 +268,7 @@ public class BukkitListener implements Listener {
         if (itemCreation != creation) return partsFound;
         String itemAuthor = PictureMeta.getAuthorUUIDString(container);
         if (!itemAuthor.equals(author)) return partsFound;
+        // We update partsFound with the inventory index since we found the part
         int[] itemParts = PictureMeta.getParts(container);
         partsFound[itemParts[0]][itemParts[1]] = inventoryIndex;
         return partsFound;
