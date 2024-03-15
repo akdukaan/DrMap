b20dc0c2373d2be73932c2a91cf3ccf04a869c54
Kaan <drbot7@gmail.com>
2024-03-14 21:43:51 -0500

    version checker doesn't need apache

--- src/main/java/org/acornmc/drmap/UpdateChecker.java
+++ src/main/java/org/acornmc/drmap/UpdateChecker.java
@@ -2,7 +2,6 @@ package org.acornmc.drmap;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -16,32 +15,16 @@ public class UpdateChecker {
 
     public static void checkForUpdates(JavaPlugin plugin) {
         // Get current version
-        String usingVersionFull = plugin.getDescription().getVersion();
-        String usingVersionClipped = usingVersionFull;
-        if (usingVersionClipped.contains("-")) {
-            usingVersionClipped = usingVersionClipped.split("-")[0];
-        }
+        String usingVersion = plugin.getDescription().getVersion();
 
         // Get latest version from Modrinth
-        final JsonElement json = getJsonAs("gpflags/" + usingVersionFull);
+        final JsonElement json = getJsonAs("gpflags/" + usingVersion);
         if (json == null) return;
-        String latestVersionFull = json.getAsJsonArray().get(0).getAsJsonObject().get("version_number").getAsString();
-        String latestVersionClipped = latestVersionFull;
-        if (latestVersionClipped.contains("-")) {
-            latestVersionClipped = latestVersionClipped.split("-")[0];
-        }
+        String latestVersion = json.getAsJsonArray().get(0).getAsJsonObject().get("version_number").getAsString();
 
-        // If on snapshot of same version
-        if (usingVersionClipped.equals(latestVersionClipped) && usingVersionFull.contains("SNAPSHOT") && !latestVersionFull.contains("SNAPSHOT")) {
-            getLogger().warning("You are using DrMap version " + usingVersionFull + " which is outdated. Please update to version " + latestVersionFull + " at https://modrinth.com/plugin/drmap.");
-            return;
-        }
-        
         // Compare more complicated versions
-        DefaultArtifactVersion usingDAV = new DefaultArtifactVersion(usingVersionClipped);
-        DefaultArtifactVersion latestDAV = new DefaultArtifactVersion(latestVersionClipped);
-        if (usingDAV.compareTo(latestDAV) < 0) {
-            getLogger().warning("You are using DrMap version " + usingVersionFull + " which is outdated. Please update to version " + latestVersionFull + " at https://modrinth.com/plugin/drmap.");
+        if (compareVersions(usingVersion,latestVersion) < 0) {
+            getLogger().warning("You are using DrMap version " + usingVersion + " which is outdated. Please update to version " + latestVersion + " at https://modrinth.com/plugin/drmap.");
         }
     }
 
@@ -60,5 +43,54 @@ public class UpdateChecker {
         }
         connection.disconnect();
         return json;
+    }
+
+    /**
+     * Compares two version strings to determine which is newer
+     * @param versionA
+     * @param versionB
+     * @return a positive number if version A is newer
+     *         0                 if versions are the same or could not be determined
+     *         a negative number if version B is newer
+     */
+    public static int compareVersions(String versionA, String versionB) {
+        // Most common case
+        if (versionA.equals(versionB)) return 0;
+
+        // Split the version strings into arrays of integers
+        String[] versionAComponents = versionA.split("\\.");
+        String[] versionBComponents = versionB.split("\\.");
+
+        // Iterate through the corresponding elements of the arrays
+        int max = Math.max(versionAComponents.length, versionBComponents.length);
+        for (int i = 0; i < max; i++) {
+            int versionANum = 0;
+            boolean aIsNumber = true;
+            if (i < versionAComponents.length) {
+                try {
+                    versionANum = Integer.parseInt(versionAComponents[i]);
+                } catch (Exception e) {
+                    aIsNumber = false;
+                    versionANum = Integer.parseInt(versionAComponents[i].replaceAll("[^0-9]", ""));
+                }
+            }
+            int versionBNum = 0;
+            boolean bIsNumber = true;
+            if (i < versionAComponents.length) {
+                try {
+                    versionBNum = Integer.parseInt(versionBComponents[i]);
+                } catch (Exception e) {
+                    bIsNumber = false;
+                    versionBNum = Integer.parseInt(versionBComponents[i].replaceAll("[^0-9]", ""));
+                }
+            }
+            int result = Integer.compare(versionANum, versionBNum);
+            if (result != 0) return result;
+            if (bIsNumber && !aIsNumber) return -1;
+            if (aIsNumber && !bIsNumber) return 1;
+        }
+
+        // They aren't the exact same but the numeric parts are the same; we can't tell.
+        return 0;
     }
 }
